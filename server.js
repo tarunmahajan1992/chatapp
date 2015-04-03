2c123e837ff475255c7d7f91e79820c75a4ddcf4
tarunmahajan1992 <tarunmahajan1992@gmail.com>
2015-04-03 23:16:37 +0530

    sever side fb

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express=require('express');
 var app=express();
 var bodyParser = require('body-parser');
 var multer = require('multer'); 
+var util = require('util');
+var session  =require('express-session');
+var cookieParser= require('cookie-parser');
+var config= require('./lib/config');
+var passport  = require('passport');
+var FacebookStrategy= require('passport-facebook').Strategy;
 
 
 var mongoose=require('mongoose');
@@ -13,9 +19,13 @@ var developerSchema=mongoose.Schema({
 
 var projectSchema=mongoose.Schema({project:String},{collection:'projects'});
 var developerModel=mongoose.model('',developerSchema);
-
+app.set('view engine', 'ejs');
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid',resave:false,saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(multer()); // for parsing multipart/form-data
 
 
@@ -68,3 +78,47 @@ app.listen(server_port, server_ip_address, function(){
         res.json(dvp);
     });
                        }
+					   
+					   
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      //Further DB code.
+      return done(null, profile);
+    });
+  }
+));
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { 
+       successRedirect : '/', 
+       failureRedirect: '/login' 
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
