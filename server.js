c847cde4ad2d64669c79c55678ef0ffe2dcc09e2
tarunmahajan1992 <tarunmahajan1992@gmail.com>
2015-04-04 14:41:31 +0530

    mvc structure change 2

--- server.js
+++ server.js
@@ -28,30 +28,5 @@ routes(app,express);
 app.listen(config.server_port, config.server_ip_address, function(){
   console.log("Listening on " + config.server_ip_address + ", server_port " + config.server_port)
 });
-
- 
-					   
-					   
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new FacebookStrategy({
-    clientID: config.facebook_api_key,
-    clientSecret:config.facebook_api_secret ,
-    callbackURL: config.callback_url
-  },
-  function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-      //Check whether the User exists or not using profile.id
-      //Further DB code.
-      return done(null, profile);
-    });
-  }
-));
-
-
+var passportConfig=require(__base+'/lib/passportConfig')(passport,FacebookStrategy,config);				   
 var passportModule=require(__base+'/controller/profileRoutes')(app,passport);
