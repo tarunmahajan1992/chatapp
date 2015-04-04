263460a95b3fbecdd8f1706280d353c4b402a90d
tarunmahajan1992 <tarunmahajan1992@gmail.com>
2015-04-04 15:38:30 +0530

    hn

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,6 @@
  var mongoose=require('mongoose');
 	var userSchema=mongoose.Schema({
-    firstname:String,id:String
+    firstname:String,id:Number
     },{collection:'users'});
 
     module.exports=mongoose.model('users',userSchema);
